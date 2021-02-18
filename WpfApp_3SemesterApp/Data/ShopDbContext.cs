3fd7741f37668dc732c0cedecfd2b4e2e444050a
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-18 21:34:55 +0100

    Add connection string with shop db context

---
+++ WpfApp_3SemesterApp/Data/ShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using WpfApp_3SemesterApp.Models;
+
+namespace WpfApp_3SemesterApp.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductRate> ProductRates { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
