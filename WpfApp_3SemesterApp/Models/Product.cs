73c78639b500cbda0907ae8c8c049b9f5c49277d
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-18 21:31:23 +0100

    Add Product model

---
+++ WpfApp_3SemesterApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WpfApp_3SemesterApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
