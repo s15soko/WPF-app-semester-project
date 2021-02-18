384e53ffade4041467c65b53ef77ae061c0eb18f
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-18 21:31:29 +0100

    Add ProductRate model

---
+++ WpfApp_3SemesterApp/Models/ProductRate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WpfApp_3SemesterApp.Models
+{
+    public class ProductRate
+    {
+        public int Id { get; set; }
+        public int Rate { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [Column("Rate")]
+        public short UserRate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
