0b8bfed16fa851b464a3c911394d515ac38626b7
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-18 21:31:34 +0100

    Add User model

---
+++ WpfApp_3SemesterApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WpfApp_3SemesterApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
