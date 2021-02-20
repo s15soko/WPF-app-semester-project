14835018079b3f08ff9a3e6c8ef558de40fe324f
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-20 12:43:08 +0100

    Add initial models migrations

---
+++ WpfApp_3SemesterApp/Migrations/202102182033452_InitialAppMigration.cs
@@ -0,0 +1,80 @@
+﻿namespace WpfApp_3SemesterApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialAppMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ProductRates",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Rate1 = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                        Rate = c.Short(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CreatedAt = c.DateTime(nullable: false),
+                        Category_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.Category_Id)
+                .Index(t => t.Category_Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ProductRates", "UserId", "dbo.Users");
+            DropForeignKey("dbo.ProductRates", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Products", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "Category_Id" });
+            DropIndex("dbo.ProductRates", new[] { "UserId" });
+            DropIndex("dbo.ProductRates", new[] { "ProductId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductRates");
+            DropTable("dbo.Categories");
+        }
+    }
+}
---
+++ WpfApp_3SemesterApp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace WpfApp_3SemesterApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WpfApp_3SemesterApp.Data.ShopDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WpfApp_3SemesterApp.Data.ShopDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
