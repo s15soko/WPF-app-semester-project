64b8f2ea19e8dcfc1c3beec8cd87f4b6ea41b356
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-20 13:20:35 +0100

    Add service for category with context service interface

---
+++ WpfApp_3SemesterApp/Services/CategoryService.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WpfApp_3SemesterApp.Data;
+using WpfApp_3SemesterApp.Models;
+
+namespace WpfApp_3SemesterApp.Services
+{
+    public class CategoryService : IDbContextService<Category>
+    {
+        public List<Category> GetAll()
+        {
+            var db = new ShopDbContext();
+            List<Category> list = (List<Category>)(from cat in db.Categories select cat);
+
+            return list;
+        }
+
+        public Category Create(Category entity)
+        {
+            var db = new ShopDbContext();
+            var newEntity = db.Categories.Add(entity);
+            db.SaveChangesAsync();
+            return newEntity;
+        }
+
+        public Category Read(int id)
+        {
+            var db = new ShopDbContext();
+            var result = db.Categories.Find(id);
+            return result;
+        }
+
+        public Category Update(Category entity)
+        {
+            var e = Read(entity.Id);
+            if(e != null)
+            {
+                var db = new ShopDbContext();
+                var updatedEntity = db.Categories.Add(entity);
+                db.SaveChanges();
+                return updatedEntity;
+            }
+
+            return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var e = Read(id);
+            if (e != null)
+            {
+                var db = new ShopDbContext();
+                db.Categories.Remove(e);
+                db.SaveChanges();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WpfApp_3SemesterApp/Services/IDbContextService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_3SemesterApp.Services
+{
+    public interface IDbContextService<T>
+    {
+        /// <summary>
+        /// Gets all existing entities.
+        /// </summary>
+        /// <returns>List of entities.</returns>
+        List<T> GetAll();
+
+        /// <summary>
+        /// Create new entity.
+        /// </summary>
+        /// <param name="entity">Entity to save.</param>
+        /// <returns>Created entity.</returns>
+        T Create(T entity);
+
+        /// <summary>
+        /// Gets entity by id.
+        /// </summary>
+        /// <param name="id">If of entity to get.</param>
+        /// <returns>Found entity.</returns>
+        T Read(int id);
+
+        /// <summary>
+        /// Update entity.
+        /// </summary>
+        /// <param name="entity">Entity to update.</param>
+        /// <returns>Updated entity.</returns>
+        T Update(T entity);
+
+        /// <summary>
+        /// Delete entity.
+        /// </summary>
+        /// <param name="id">If of entity to delete.</param>
+        /// <returns>Whether action was successful.</returns>
+        bool Delete(int id);
+    }
+}
