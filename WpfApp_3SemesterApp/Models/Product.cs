003108a56c9f1dc7ba9ee9469aca0d1d420ab906
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-21 15:48:53 +0100

    Add CategoryId (with migration) to product model

--- WpfApp_3SemesterApp/Models/Product.cs
+++ WpfApp_3SemesterApp/Models/Product.cs
@@ -7,6 +7,7 @@ namespace WpfApp_3SemesterApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int CategoryId { get; set; }
         public Category Category { get; set; }
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
