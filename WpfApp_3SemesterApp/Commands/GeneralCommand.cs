5f981ffc026785f75236b72c04bdb70c6b952929
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-02-21 13:31:33 +0100

    Add GeneralCommand (ICommand)

---
+++ WpfApp_3SemesterApp/Commands/GeneralCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp_3SemesterApp.Commands
+{
+    public class GeneralCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action DoWork;
+
+        public GeneralCommand(Action work)
+        {
+            DoWork = work;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            DoWork();
+        }
+    }
+}
